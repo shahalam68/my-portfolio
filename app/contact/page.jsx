8e42f7e76d2df4b6c2cfdba2f10e7399760a9f5c
shshalam68 <a.k.mshahalam68@gmail.com>
2024-07-10 19:46:12 +0600

    contact created

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -45,7 +45,7 @@ const Contact = () => {
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
@@ -54,15 +54,48 @@ const Contact = () => {
               </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type="firstname" placeholder="Firstname"/>
-                <Input type="lastname" placeholder="Lastname"/>
-                <Input type="email" placeholder="Email"/>
-                <Input type="phone" placeholder="Phone number"/>
+                <Input type="firstname" placeholder="Firstname" />
+                <Input type="lastname" placeholder="Lastname" />
+                <Input type="email" placeholder="Email" />
+                <Input type="phone" placeholder="Phone number" />
               </div>
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">Web Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              {/* text area */}
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type Your message here."
+              />
+              {/* btn */}
+              <Button size="md">Send message</Button>
             </form>
           </div>
           <div className="flex-1 flex items-center xl:justify-end xl:order-none order-1 mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6 ">
+                    <div className="w-[52px] h-[52px] xl:h-[72px] xl:w-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px] ">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
